6ec653f2dd23f777d9af7dfaed1f35b16c2b654b
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-03-29 02:16:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
 }

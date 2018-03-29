aba7f01c71fb0801cec615af2dbceca3477ebea8
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-03-29 02:26:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown( , holiday)
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
